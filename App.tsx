980784291f145d1caeedd02af6ce2248b48377a1
Lucas Leib <lucas.leib@gmail.com>
2024-02-05 14:09:24 +0100

    feat : add the logical

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 //Form validation
 import * as Yup from 'yup'
@@ -7,11 +7,66 @@ import * as Yup from 'yup'
 const PasswordSchema = Yup.object().shape({
   passwordLength: Yup.number()
     .required('Required')
-    .min(4, 'Too short')
-    .max(16, 'Too long')
+    .min(4, 'Should be 4 characters minimum')
+    .max(16, 'Should be 16 characters maximum'),
 })
 
 export default function App() {
+
+  const [password, setPassword] = useState('')
+  const [isPassGenerated, setIsPassGenerated] = useState(false)
+
+  const [lowerCase, setLowerCase] = useState(true)
+  const [upperCase, setUpperCase] = useState(false)
+  const [numbers, setNumbers] = useState(false)
+  const [symbols, setSymbols] = useState(false)
+
+  const generatePassswordString = (passwordLength: number) => {
+    let characterList = ''
+    
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz'
+    const digitalChars = '0123456789'
+    const specialChars = '!@#$%^&*()_+'
+
+    if (upperCase) {
+      characterList += upperCaseChars
+    }
+    if (lowerCase) {
+      characterList += lowerCaseChars
+    }
+    if (numbers) {
+      characterList += digitalChars
+    }
+    if (symbols) {
+      characterList += specialChars
+    }
+
+    const passwordResult = createPassword(characterList, passwordLength)
+
+    setPassword(passwordResult)
+    setIsPassGenerated(true)
+
+  }
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = ''
+    for (let i=0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length)
+      result += characters.charAt(characterIndex)
+    }
+    return result
+  }
+
+  const resetPasswordState = () => {
+    setPassword('')
+    setIsPassGenerated(false)
+    setLowerCase(false)
+    setUpperCase(false)
+    setNumbers(false)
+    setSymbols(false)
+  }
+
   return (
     <View>
       <Text>App</Text>
